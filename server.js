b597423eaf86aa3419e1fcbbfd9e18734fb05540
Adel Mahjoub <dragonhunter250@gmail.com>
2017-07-14 16:55:43 +0200

    added x-forwarded-for

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req, res, next) => {
   let language = req.headers['accept-language'].split(';')[0].split('-')[0];
-  let ip_address = req.headers['x-forwarded-by'] ? req.headers['x-forwarded-by'].split(',')[0] : 'localhost';
+  let ip_address = req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'].split(',')[0] : 'localhost';
   let system = req.headers['user-agent'].split(' ')[2];
   let architecture = req.headers['user-agent'].split(' ')[3].replace(')','');
   res.json({
